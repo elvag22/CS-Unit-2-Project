57b3834c8e0c05fd8067aaed846131019963f971
elvag22 <elva.gao@thekaustschool.org>
2025-11-26 22:12:03 +0300

    Create Main.java

---
+++ Project 2/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CarCongestionAnalyze {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("Car Counter (c_s) - Sheet1.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+
+}
