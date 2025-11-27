e636d67be5b1de4b688907fcc8bc8361b99a1b12
elvag22 <elva.gao@thekaustschool.org>
2025-11-27 11:08:04 +0300

    Update CarCongestionAnalyze.java

--- Project 2/src/CarCongestionAnalyze.java
+++ Project 2/src/CarCongestionAnalyze.java
@@ -5,10 +5,14 @@ import java.util.ArrayList;
 
 public class CarCongestionAnalyze {
     public static void main(String[] args) {
+        ArrayList<Integer> northbound = new ArrayList<>();
+        ArrayList<Integer> southbound = new ArrayList<>();
+        ArrayList<Integer> eastbound  = new ArrayList<>();
+        ArrayList<Integer> westbound  = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader("Car Counter (c_s) - Sheet1.csv"))) {
             String line;
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
+
             }
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
