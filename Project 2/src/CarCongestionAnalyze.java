639ef4c837f9dbfc7d1e65c95b36f69d7d774992
elvag22 <elva.gao@thekaustschool.org>
2025-11-29 12:59:22 +0300

    Update CarCongestionAnalyze.java

--- Project 2/src/CarCongestionAnalyze.java
+++ Project 2/src/CarCongestionAnalyze.java
@@ -7,8 +7,8 @@ public class CarCongestionAnalyze {
     public static void main(String[] args) {
         ArrayList<Integer> northbound = new ArrayList<>();
         ArrayList<Integer> southbound = new ArrayList<>();
-        ArrayList<Integer> eastbound  = new ArrayList<>();
-        ArrayList<Integer> westbound  = new ArrayList<>();
+        ArrayList<Integer> eastbound = new ArrayList<>();
+        ArrayList<Integer> westbound = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader("Car Counter (c_s) - Sheet1.csv"))) {
             String line;
             boolean first = true;
@@ -16,7 +16,7 @@ public class CarCongestionAnalyze {
                 if (first == true) {
                     first = false;
                 } else {
-                    int firstcomma =  line.indexOf(',');
+                    int firstcomma = line.indexOf(',');
                     int secondcomma = line.indexOf(',', firstcomma + 1);
                     int thirdcomma = line.indexOf(',', secondcomma + 1);
 
@@ -40,37 +40,52 @@ public class CarCongestionAnalyze {
         read("Eastbound", eastbound);
         read("Westbound", westbound);
     }
+
     public static void read(String name, ArrayList<Integer> list) {
         System.out.println("-{" + name + "}-");
+        double average = average(list);
+        System.out.println("Average of cars is: " + average);
+        if (average >= 30)
+            System.out.println("Traffic Level is high right now");
+        else
+            System.out.println("Traffic Level is low right now");
+        double slope = slope(list);
+        System.out.println("The trend of cars right now is " + slope);
+
+        System.out.println("Predicted next 5 minutes:");
+        predicton(list, slope);
+        lightsuggestion(average, slope);
+
+
     }
+
     public static double average(ArrayList<Integer> list) {
         double sum = 0;
         for (double value : list) sum += value;
         return (double) sum / list.size();
     }
+
     public static double slope(ArrayList<Integer> list) {
         int lastnum = list.get(list.size() - 1);
         int secondlastnum = list.get(list.size() - 2);
         return (secondlastnum - lastnum);
 
     }
+
     public static void predicton(ArrayList<Integer> list, double slope) {
         int last = list.get(list.size() - 1);
         for (int i = 1; i <= 5; i++) {
             double predicted = last + (slope * i);
-            System.out.println("It's predicted that there will be" + i + "cars: " + predicted);
-
-
+            System.out.println("Predicted cars in " + i + " minutes: " + predicted);
 
         }
     }
+
     public static void lightsuggestion(double average, double slope) {
-        int greeTLight;
-
+        int greenLight;
+        if (average >= 35) greenLight = 50;
+        else if (average >= 25) greenLight = 40;
+        else greenLight = 25;
+        System.out.println("Recommended green light is: " + greenLight + " seconds");
     }
-
-
-
-
-
-    }
+}
